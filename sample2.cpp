665984e8f36f394d8793447665415fb4c881f4c4
Keiji Kimura <kkimura2317@gmail.com>
2019-10-14 00:41:09 +0900

    fix a few comments

--- sample2.cpp
+++ sample2.cpp
@@ -9,23 +9,23 @@ class Memo {
    int val;
    public:
       Memo( int a ) { val = a; }
-      auto getVal() { return val; } // auto: 返り値を予測 -> int
+      auto getVal() { return val; } // auto: 返り値の型を推論 -> int
 };
 
 int main( int argc, char** argv){
 
-   auto x = 1; // auto: xの型を予測 -> int
-   auto p = &x; // auto: yの型を予測 -> int*
+   auto x = 1; // auto: xの型を推論 -> int
+   auto p = &x; // auto: yの型を推論 -> int*
 
    *p = 2;
 
    std::vector<int> vec( 3 ); // サイズ3のベクトルを生成
 
-   // auto: イテレータの型名をautoで予測 -> std::vector<int>::iterator
+   // auto: イテレータの型名をautoで推論 -> std::vector<int>::iterator
    for ( auto it = vec.begin(); it != vec.end(); ++it )
       *it = x++;
 
-   // auto: コンテナの要素を予測 -> int&
+   // auto: コンテナの要素を推論 -> int&
    for ( auto& v : vec )
    {
       Memo m( v*v ); // 2乗してメモ
